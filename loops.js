cf487826f41c97ebe90737583638754ecee80f3b
Nathan Hill <nateohill@gmail.com>
2018-05-19 00:31:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
 function forLoop(array) {
-  let arr = [];
   for(let i = 0; i < 25; i++) {
     if(i === 1) {
-      arr.push("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     } else {
-      arr.push(`I am ${i} strange loops`);
+      array.push(`I am ${i} strange loops`);
     }
   }
-  return arr;
+  return array;
 }
